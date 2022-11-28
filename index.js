46f459d3003991ba3f7e3968b354bf0a2cb6c4db
tiia90 <62059457+tiia90@users.noreply.github.com>
2022-11-28 11:56:48 +0200

    heroku

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.post("/addcustomer", (req, res) => {
   res.redirect("/");
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${port}.`);
   });
